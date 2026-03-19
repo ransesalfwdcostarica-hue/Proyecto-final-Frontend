85486b8efd6cd9d1a8052ba433c1cab514b710b2
ransesalfwdcostarica-hue <ransesalfwdcostarica@gmail.com>
2026-03-19 11:08:54 -0600

    commit

--- src/Pages/DashboardAdmin.jsx
+++ src/Pages/DashboardAdmin.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import DashAdmin from '../components/DashAdmin';
+import DashboardAdministrador from '../components/DashboardAdministrador';
 
 const DashboardAdmin = () => {
-  return <DashAdmin />;
+  return <DashboardAdministrador />;
 };
 
 export default DashboardAdmin;
