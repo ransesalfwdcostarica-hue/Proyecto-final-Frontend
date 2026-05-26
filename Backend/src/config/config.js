e15a3d1f731723a6cab908a40f86390f29e345b2
asporras7-dev <asporras7@gmail.com>
2026-05-26 10:34:59 -0600

    Cambios

--- Backend/src/config/config.js
+++ Backend/src/config/config.js
@@ -2,18 +2,12 @@ require('dotenv').config({ path: require('path').resolve(__dirname, '../../.env'
 
 module.exports = {
     development: {
-<<<<<<< HEAD
-        username: process.env.DB_USER || "parrillada",
-=======
         username: process.env.DB_USER || "root",
->>>>>>> 2f9846f10d2606fce96d7154245e4461b48792cf
         password: process.env.DB_PASSWORD || "root",
         database: process.env.DB_NAME || "powerfit",
         host: process.env.DB_HOST || "localhost",
         dialect: process.env.DB_DIALECT || "mysql"
     },
-<<<<<<< HEAD
-=======
     db:{
         name: process.env.DB_NAME || "powerfit",
         user: process.env.DB_USER || "root",
@@ -22,7 +16,6 @@ module.exports = {
         dialect: process.env.DB_DIALECT || "mysql",
         storage: process.env.DB_STORAGE || "./database.sqlite"
     },
->>>>>>> 2f9846f10d2606fce96d7154245e4461b48792cf
     server: {
         port: process.env.PORT || 3000
     },
